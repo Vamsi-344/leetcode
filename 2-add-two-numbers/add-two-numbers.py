0e4492f0fe6b8e08ba2e0890af1d96d28e72c092
Vamsi Krishna Sethu <82080571+Vamsi-344@users.noreply.github.com>
2024-11-07 20:36:35 +0530

    Time: 4 ms (53.14%) | Memory: 16.7 MB (54.72%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -4,32 +4,26 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def getTheNumber(self, ll: Optional[ListNode]) -> int:
-        count = 0
-        if ll == None:
-            return count
-        itr = ll
-        res = 0
-        while itr.next:
-            res += (itr.val)*(10**(count)) 
-            itr = itr.next
+    def liValue(self, l: Optional[ListNode]) -> int:
+        count, res, itr = 0, 0, l
+        while itr.next != None:
+            res += (itr.val*(10**(count)))
             count += 1
-        res += (itr.val)*(10**(count)) 
+            itr = itr.next
+        res += itr.val*10**(count)
         return res
     
-    def createAList(self, s: int) -> Optional[ListNode]:
-        ll = ListNode(int(s[0]), None)
-        itr = ll
-        for i in s[1:]:
-            itr.next = ListNode(int(i), None)
-            itr = itr.next
-        return ll
-
+    def numToLinkedList(self, x: int) -> Optional[ListNode]:
+        head = None
+        temp = head
+        for i in str(x):
+            temp = ListNode(int(i))
+            temp.next = head
+            head = temp
+        return head
 
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        a = self.getTheNumber(l1)
-        b = self.getTheNumber(l2)
-        c = a + b
-        d = str(c)
-        e = d[::-1]
-        return self.createAList(e)
+        req = self.liValue(l1) + self.liValue(l2)
+        return self.numToLinkedList(req)
+
+        
